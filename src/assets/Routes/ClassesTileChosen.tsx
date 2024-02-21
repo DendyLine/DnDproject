62b6f7fd63e437d704d3126285444a8cf8314996
DendyLine <anatolijrivne@gmail.com>
2024-02-21 19:16:49 +0200

    start to develop classes

--- src/assets/Routes/ClassesTileChosen.tsx
+++ src/assets/Routes/ClassesTileChosen.tsx
@@ -1,7 +1,13 @@
+import { useParams } from 'react-router-dom';
+import { useAppSelector } from '../redux/store';
+
 
 export const ClassesTileChosen = () => {
+    const { id } = useParams();
+    const classes = useAppSelector(state => state.classes.tiles);
+    const oneClass = classes.find(oneClass => oneClass.id === id);
     return(
-        <div className='RacesTileChosen'>
+        <div className='oneClass'>
             hello world
         </div>
     )
--- src/assets/markdown/races/index.ts
+++
@@ -1,9 +0,0 @@
-import './aarakocra.mdx'
-// @ts-ignore
-import {raceDescriptions} from '../races/descriptions.js'
-
-
-export {
-    raceDescriptions
-
-}
